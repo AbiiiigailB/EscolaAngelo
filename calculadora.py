d32e24021f96b3a35c08b867756aa3c9e72ea209
AbiiiigailB <abigail@zonaleste.net>
2024-05-29 08:59:53 -0300

    calculadoraa

---
+++ calculadora.py
@@ -0,0 +1,20 @@
+def calcular(x , y)
+    
+    print("a soma é:", x + y)
+    if(op == 1)
+    print("o resto é:", x - y)
+    elif(op == 2)
+    print("o produto é:", x * y)
+    elif(op == 3)
+    print("o quociente é:", x / y)
+    elif(op == 4)
+    print("o resto da divisão é:", x % y)
+    elif(op == 5)
+    print("a potência é:", x ** y)
+    elif(op == 6)
+
+
+
+x = int(input("digite um número"))
+y = int(input("digite outro número"))
+calcular(x , y)
---
+++ dicionario@.py
@@ -0,0 +1,9 @@
+pais = {'nome' : 'brasil', 'capital' : 'brasil'}
+print(pais)
+print(pais['capital'])
+#pais.clear()
+
+#metodo get()
+
+print(pais.get("nome", "não existe"))
+print(pais.get("nome", "não existe"))
